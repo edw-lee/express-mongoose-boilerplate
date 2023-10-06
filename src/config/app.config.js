4db2c6c5e5db8d47d6f3b3c79c52cfe05b7dc394
Edwin Lee <edwinlee107@hotmail.my>
2023-10-06 09:46:35 +0800

    Update .env.sample and app.config.js

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
-dotenv.config('.env');
+dotenv.config({ path: process.env.NODE_ENV != 'production' ? `.env.${process.env.NODE_ENV}` : '.env' });
 
 export const port = process.env.PORT;
 export const mongoDBConnectionString = process.env.MONGODB_CONNECTION_STRING;
